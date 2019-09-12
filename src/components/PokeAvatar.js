a055b9d402ab1ac668bee8a446b8fa0822020bcb
Taric <174430@student.ue.wroc.pl>
2019-09-12 22:32:30 +0200

    day #3 - connected to API, fixed PokeList layout, displaying names

--- src/components/PokeAvatar.js
+++ src/components/PokeAvatar.js
@@ -1,10 +1,29 @@
-import React, { Component } from 'react'
-import './styles/PokeAvatar.css'
+import React, { Component } from 'react';
+import './styles/PokeAvatar.css';
 
 export default class PokeAvatar extends Component {
+
+  state = {
+    pokemonIndex: '',
+    imageUrl: '',
+    name: '',
+  }
+  
+
+  componentDidMount() {
+    const {url, name} = this.props;
+    const pokemonIndex = url;
+
+    this.setState ({
+      pokemonIndex: pokemonIndex,
+      name,
+  //    imageUrl: imageUrl
+    })
+  }
+
   render() {
     return (
-      <button id='image' className='avatar'></button> 
+      <div className='avatar'> <p> {this.state.name} </p> </div>
     )
   }
 }
--- src/components/PokeList.js
+++ src/components/PokeList.js
@@ -1,22 +1,35 @@
 import React from 'react';
-import './styles/PokeList.css'
-import './PokeAvatar.js'
+import './styles/PokeList.css';
 import PokeAvatar from './PokeAvatar.js';
+import Axios from 'axios';
 
 class PokeList extends React.Component {
+
+    state = {
+      url: 'https://pokeapi.co/api/v2/pokemon/',
+      pokemon: null
+    }
+
+  
+
+  async componentDidMount() {
+    const res = await Axios.get(this.state.url);
+    this.setState({pokemon: res.data['results'] });
+  }
+
   render() {
     return (
-      <div className='list-container'> 
-          <PokeAvatar />
-          <PokeAvatar />
-          <PokeAvatar />
-          <PokeAvatar />
-          <PokeAvatar />
-          <PokeAvatar />
-          <PokeAvatar />
-          <PokeAvatar />
-          <PokeAvatar />
-      </div>
+      <>
+        {this.state.pokemon ? (<div className='list-container'> 
+          {this.state.pokemon.map(pokemon => <PokeAvatar 
+            key={pokemon.name}
+            url={pokemon.url}
+            name={pokemon.name}
+            image={pokemon.sprites}          
+          />)}
+        </div>) : ("asdas")
+        }
+      </>
     )
   }
 }
